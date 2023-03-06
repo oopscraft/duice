b37b41ecc7ef36a5fc21a7b46ff80d2439f826c7
chomookun <chomookun@gmail.com>
2023-03-07 02:51:46 +0900

    .

---
+++ test/DataTest.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+import vm from "vm";
+import { JSDOM } from 'jsdom';
+vm.runInThisContext(fs.readFileSync("../dist/duice.js"));
+let jdom = new JSDOM('<!DOCTYPE html>');
+global.window = jdom.window;
+global.document = jdom.window.document;
+
+// create
+let myData = duice.Data.create({
+    name: 'Apple'
+});
+console.log(myData);
+
+
+
