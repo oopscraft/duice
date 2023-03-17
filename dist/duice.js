14205bf4a614bc86f32d84ca1b29f5c6d2c08abd
chomookun <chomookun@gmail.com>
2023-03-18 01:07:01 +0900

    .

--- dist/duice.js
+++ dist/duice.js
@@ -1293,7 +1293,13 @@ var duice;
      * Generates component ID
      */
     function generateId() {
-        return self.crypto.randomUUID();
+        let dt = new Date().getTime();
+        let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = (dt + Math.random() * 16) % 16 | 0;
+            dt = Math.floor(dt / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
     }
     duice.generateId = generateId;
     /**
