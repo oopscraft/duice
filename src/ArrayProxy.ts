5873ac6b4ca09b22f1ddd4b0e7df083e5807979c
chomookun <chomookun@gmail.com>
2023-05-13 23:08:40 +0900

    .

--- src/ArrayProxy.ts
+++ src/ArrayProxy.ts
@@ -18,9 +18,9 @@ namespace duice {
             if(globalThis.Array.isArray(array)){
                 array.forEach((object, index) => {
                     let objectProxy = new ObjectProxy(object);
-                    //ObjectProxy.getHandler(objectProxy).addObserver(arrayHandler);
+                    ObjectProxy.getHandler(objectProxy).addObserver(arrayHandler);
                     let a = ObjectProxy.getHandler(objectProxy);
-                    //console.log(a);
+                    console.log(a);
                     // TODO test
                     //this[index] = objectProxy;
                 });
