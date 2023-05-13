2e1cf2a52945be4cce345ef71380d9ed4e43b0fd
chomookun <chomookun@gmail.com>
2023-05-13 21:46:24 +0900

    .

--- dist/duice.js
+++ dist/duice.js
@@ -3076,6 +3076,8 @@ var duice;
                 array.forEach((object, index) => {
                     let objectProxy = new duice.ObjectProxy(object);
                     //ObjectProxy.getHandler(objectProxy).addObserver(arrayHandler);
+                    let a = duice.ObjectProxy.getHandler(objectProxy);
+                    console.log(a);
                     // TODO test
                     //this[index] = objectProxy;
                 });
--- src/ArrayProxy.ts
+++ src/ArrayProxy.ts
@@ -19,6 +19,8 @@ namespace duice {
                 array.forEach((object, index) => {
                     let objectProxy = new ObjectProxy(object);
                     //ObjectProxy.getHandler(objectProxy).addObserver(arrayHandler);
+                    let a = ObjectProxy.getHandler(objectProxy);
+                    console.log(a);
                     // TODO test
                     //this[index] = objectProxy;
                 });
